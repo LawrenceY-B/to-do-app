b6fe41fb4ea68040ac86dc90218342791b5c7c7c
Lawrence Yirenkyi-Boafo <lawrencekyb@gmail.com>
2023-04-29 19:33:42 +0000

    minor change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ITODOdata } from 'src/assets/models/todo.model';
 import { v4 as uuidv4 } from 'uuid';
 @Component({
@@ -21,10 +21,41 @@ export class AppComponent implements OnInit{
   results: ITODOdata[] = []
   tempresults: ITODOdata[] = []
 
-  constructor() {
+  circle!: HTMLDivElement;
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    this.circle.style.top = y + -13+ 'px';
+    this.circle.style.left = x + -10+'px';
     
+    const cursorSize = 60;
+    const target = event.target as HTMLElement;
+    const isLink = target.tagName === 'P';
+
+    if (isLink) {
+      this.circle.style.width = cursorSize + -13 + 'px';
+      this.circle.style.height = cursorSize + -10+'px';
+      this.circle.style.top = y + -23+ 'px';
+      this.circle.style.left = x + -20+'px';
+      this.circle.style.border = '3px solid white';
+      this.circle.style.mixBlendMode = 'difference';
+    } else {
+      this.circle.style.width = '20px';
+      this.circle.style.height = '20px';
+      this.circle.style.border = 'none';
+      this.circle.style.mixBlendMode = 'normal';
+    }
+
+  
   }
   ngOnInit(): void {
+    const circleElement = document.querySelector('.circle');
+    if (circleElement) {
+      this.circle = circleElement as HTMLDivElement;
+    }
     this.detectColorScheme();
   }
   detectColorScheme(){
